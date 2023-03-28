6ecdb5a728bae58e7ffe880e714ed0a62905621b
lukaskovalik <128225231+lukaskovalik@users.noreply.github.com>
2023-03-28 06:10:12 +0200

    Initial commit

--- rps_game.py
+++ rps_game.py
@@ -29,14 +29,8 @@ while True:
         print("You won!")
         user_wins += 1
 
-    elif user_input == "scissors" and computer_pick == "scissors":
-        print("Nobody won! Again!")
-
-    elif user_input == "rock" and computer_pick == "rock":
-        print("Nobody won! Again!")
-
-    elif user_input == "paper" and computer_pick == "paper":
-        print("Nobody won! Again!")
+    elif user_input == computer_pick:
+        print("It's a tie! Again!")
 
     else:
         print("You lose!")
