3db76928366e8a1e4ed9c03ce773788b2c88caf5
lukaskovalik <128225231+lukaskovalik@users.noreply.github.com>
2023-03-29 14:58:24 +0200

    Initial commit

---
+++ password_manager.py
@@ -0,0 +1,38 @@
+from cryptography.fernet import Fernet
+
+master_pwd = input("What is your master password? ")
+
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write()'''
+
+def view():
+    with open('password.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("User: ", user, ", Password: ", passw)
+
+def add():
+    name = input("Account name: ")
+    pwd = input("Password: ")
+
+    with open('password.txt', 'a') as f:# a-add,v-view,w-write,r-read, atd
+        f.write(name + "|" + pwd + "\n")
+
+while True:
+    mode = input("Would you like to add new password or view existing ones (view, add)? Or quit? (Q): ").lower()
+
+    if mode == "q":
+        break
+
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Wrong input")
+        continue
+
--- quiz.py
+++ quiz.py
@@ -1,1 +1,30 @@
+name = input("Type your name: ")
+print("Welcome", name, "to this adventure!")
 
+answer = input("You are on a dirt road. You can go right or left, infront crossroad. "
+                "\nWhich way do you wanna go: ").lower()
+if answer == "left":
+    answer = input("You come to the river, you can walk around or swim \nWhat do you choose? ").lower()
+    if answer == "swim":
+        print("You swam accros and were eaten by a giant fish")
+    elif answer == "walk" or "walk around":
+        print("You walk for many miles, ran out of water and you lost game.")
+    else:
+        print("Wrong input")
+elif answer == "right":
+    answer = input("You came to the bridge that looks wobly. \nYou wanna cross or go back? (cross/back)")
+    if answer == "cross":
+        answer = input("You cross the bridge and met stranger. \nDo you talk to him? (yes/no)")
+        if answer == "yes":
+            print("You talk to the stranger and win the game, gold, medal and all shit.")
+        elif answer == "no":
+            print("Stranger get offended and you lose.")
+        else:
+            print("Wrong input")
+    elif answer == "back":
+        print("You go back to main road and nothing more happend.")
+    else:
+        print("Wrong input")
+else:
+    print("Wrong input")
+print("thanx for the time you wasted on this game")
