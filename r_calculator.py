236feaff279c67d2ff4d06983069fa1a3e559de1
lukaskovalik <128225231+lukaskovalik@users.noreply.github.com>
2023-04-25 09:43:41 +0200

    Initial commit

--- r_calculator.py
+++ r_calculator.py
@@ -2,7 +2,14 @@
 lenght = input("Vzdialenost v km: \n").replace(',','.')
 pace = input("Priemerne tempo behu: \n")
 minuty, sekundy = pace.split(':')
+
 vypocetCasu = (int(minuty)*60 + int(sekundy))*float(lenght)
-behMinuty = int(vypocetCasu // 60)
 behSekundy = int(vypocetCasu % 60)
-print("Cas behu " + str(behMinuty) + "m:" + str(behSekundy) + "s")
+behMinuty = int(vypocetCasu // 60)
+
+if behMinuty > 59:
+    behHodiny = behMinuty // 60
+    behMinutyIf = behMinuty % 60
+    print("Cas behu " + str(behHodiny) + "h:" + str(behMinutyIf) + "m:" + str(behSekundy) + "s")
+else:
+    print("Cas behu " + str(behMinuty) + "m:" + str(behSekundy) + "s")
