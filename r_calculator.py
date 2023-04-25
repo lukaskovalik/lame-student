b5d8a5df0fa13d6ae7d830a16bea717b0ce229fd
lukaskovalik <128225231+lukaskovalik@users.noreply.github.com>
2023-04-25 10:26:51 +0200

    Initial commit

--- r_calculator.py
+++ r_calculator.py
@@ -1,15 +1,38 @@
-#pace calculator
-lenght = input("Vzdialenost v km: \n").replace(',','.')
-pace = input("Priemerne tempo behu: \n")
-minuty, sekundy = pace.split(':')
+#runnin pace calculator
 
-vypocetCasu = (int(minuty)*60 + int(sekundy))*float(lenght)
-behSekundy = int(vypocetCasu % 60)
-behMinuty = int(vypocetCasu // 60)
+while True:
+    print("Napis 1 ak chces vypocitat priemerne tempo behu")
+    print("Napis 2 ak chces vypocitat cas behu")
+    print("Napis 3 ak chces ukoncit program")
+    userInput = int(input())
+    if userInput == 1:
+        lenght = input("Zadaj vzdialenost v km: \n").replace(',', '.')
+        time = input("Zadaj cas behu: \n")
+        if time.count(':') == 2:
+            hodiny, minuty, sekundy = time.split(':')
+        else:
+            hodiny = 0
+            minuty, sekundy = time.split(':')
+        totalSec = (int(hodiny) * 3600) + (int(minuty) * 60) + int(sekundy)
+        secPerKm = float(totalSec) / float(lenght)
+        paceMin = int(secPerKm // 60)
+        paceSec = int(secPerKm % 60)
+        print("Priemerne tempo behu " + str(paceMin) + ":" + str(paceSec)+" \n")
 
-if behMinuty > 59:
-    behHodiny = behMinuty // 60
-    behMinutyIf = behMinuty % 60
-    print("Cas behu " + str(behHodiny) + "h:" + str(behMinutyIf) + "m:" + str(behSekundy) + "s")
-else:
-    print("Cas behu " + str(behMinuty) + "m:" + str(behSekundy) + "s")
+    elif userInput == 2:
+        lenght = input("Vzdialenost v km: \n").replace(',', '.')
+        pace = input("Priemerne tempo behu: \n")
+        minuty, sekundy = pace.split(':')
+        vypocetCasu = (int(minuty)*60 + int(sekundy))*float(lenght)
+        behSekundy = int(vypocetCasu % 60)
+        behMinuty = int(vypocetCasu // 60)
+        if behMinuty > 59:
+            behHodiny = behMinuty // 60
+            behMinutyIf = behMinuty % 60
+            print("Cas behu " + str(behHodiny) + "h:" + str(behMinutyIf) + "m:" + str(behSekundy) + "s \n")
+        else:
+            print("Cas behu " + str(behMinuty) + "m:" + str(behSekundy) + "s \n")
+    elif userInput == 3:
+        quit()
+    else:
+        continue
